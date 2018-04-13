5e0d50a10b14f7ed4b90bfa117aedb9b534a4283
Olivier ESUKA <oesukam@gmail.com>
2018-04-13 12:49:52 +0800

    project completed

---
+++ README.md
@@ -0,0 +1,19 @@
+# Final Year Project Wuhan University
+
+## System functionalities:
+
++ Login and logout, forget password.
+
++ Manage employees (Add, edit and delete).
+
++ Manage countries, cities, states (Add, edit and delete).
+
++ Manage users (Add, edit and delete).
+
++ Manage division (Add, edit and delete).
+
++ Manage departments (Add, edit and delete).
+
++ Make reports (export PDF).
+
++ Search (with multiple combine fields).
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $user = factory(App\User::class)->create([
+             'username' => 'admin',
+             'email' => 'admin@gmail.com',
+             'password' => bcrypt('admin'),
+             'lastname' => 'Ms',
+             'firstname' => 'admin'
+         ]);
+        // $country = factory(App\Country::class)->create([
+        //     'country_code' => 'CN',
+        //     'name' => 'China',
+        // ]);
+        $department = factory(App\Department::class)->create([
+            'name' => 'IT',
+        ]);
+    }
+}
